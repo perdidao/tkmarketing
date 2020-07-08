bd19c76df630b78a6eaf56eac1fa0c8a52b4ef09
Lucas Almeida <lucasecalmeida@gmail.com>
2020-07-08 18:17:14 -0300

    Página do post

--- single.php
+++ single.php
@@ -9,12 +9,16 @@
 
 <section class="page-header">
   <div class="container">
-    <h1 class="page-header__title">
+    <h1 class="page-header__title post__title">
       <?php the_title(); ?>
     </h1>
     <div class="page-header__image" style="background-image:url(<?php the_post_thumbnail_url('full'); ?>)"></div>
   </div>
 </section>
+
+<figure class="post__image">
+  <img src="<?php the_post_thumbnail_url('full') ?>" alt="<?php the_title(); ?>">
+</figure>
 
 <main class="post__container container">
   <?php the_content(); ?>
